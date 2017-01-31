9e696315cf251025c2b856be76c91e7b2752049e
FabricioArturo <FabricioArturo@192.168.1.76>
2017-01-31 12:29:41 -0600

    Problema 2.2, Tarea 1

--- src/spp/t01/p2/SPPT01P2.java
+++ src/spp/t01/p2/SPPT01P2.java
@@ -1,4 +1,4 @@
-/*Problema 2 de Fabricio Arturo Balboa Cavazos A01411541
+/*//Problema 2 de IMT Fabricio Arturo Balboa Cavazos A01411541
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
